13a3382f76a2c4539f65e1f3bea1fdf8e5bd5a57
claire Roughan <claire_roughan@yahoo.co.uk>
2020-03-03 19:56:40 +0000

    Initial Commit

---
+++ MovieBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBooking
+//
+//  Created by claire.roughan on 03/03/2020.
+//  Copyright © 2020 claire.roughan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
